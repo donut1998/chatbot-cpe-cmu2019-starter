ffd39c3ac685dc7e7b38ca78f0e44cad16c84f0d
donut1998 <donut.3021998@gmail.com>
2019-02-02 14:14:46 +0700

    Update server.js

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.get('/', function (req, res) {
 
 app.post('/webhook', middleware(config), (req, res) => {
   res.send('hello!!')
-  const event = req.body.events[0];
-    
+  const event = req.body.events[0]; //เก็บข้อความ,สติ๊กเกอร์
+  text: event.type
   if (event.type === 'message') {
     const message = event.message;
     console.log(message)
