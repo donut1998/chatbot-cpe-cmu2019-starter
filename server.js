9c4b8badab170da8880aedf4e8437d4999209615
donut1998 <donut.3021998@gmail.com>
2019-02-02 11:21:57 +0700

    c5

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express')
 const middleware = require('@line/bot-sdk').middleware
 const app = express()
 
-const config = {
-  channelAccessToken: 'hQWQDyh2yengS8JSCPVb5JZZj+KhRuVHvRCL+W9XVNJnuvbwphLneuu3bcOz4UOOzSc1RSd8ZCqh9wq5dvGZvckSr3fxl50LBLV981Q4S4gVyQ67WGwFpFRq+PLSbk3GrgpCt5OKn+aPGmYo16KNRAdB04t89/1O/w1cDnyilFU=',
-  channelSecret: '1614fdc372087e9c6a6fc36829fad2f2'
-}
+// const config = {
+//   // channelAccessToken: 'hQWQDyh2yengS8JSCPVb5JZZj+KhRuVHvRCL+W9XVNJnuvbwphLneuu3bcOz4UOOzSc1RSd8ZCqh9wq5dvGZvckSr3fxl50LBLV981Q4S4gVyQ67WGwFpFRq+PLSbk3GrgpCt5OKn+aPGmYo16KNRAdB04t89/1O/w1cDnyilFU=',
+//   // channelSecret: '1614fdc372087e9c6a6fc36829fad2f2'
+// }
 
 const app = express()
 
