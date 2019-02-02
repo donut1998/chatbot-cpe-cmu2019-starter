7cbe06f5dbc9dac351b6831510ae65decfccd881
donut1998 <donut.3021998@gmail.com>
2019-02-02 11:32:18 +0700

    c7

--- server.js
+++ server.js
@@ -1,21 +1,18 @@
 const express = require('express')
-// const middleware = require('@line/bot-sdk').middleware
+const middleware = require('@line/bot-sdk').middleware
 const app = express()
 
-// const config = {
-//   // channelAccessToken: 'hQWQDyh2yengS8JSCPVb5JZZj+KhRuVHvRCL+W9XVNJnuvbwphLneuu3bcOz4UOOzSc1RSd8ZCqh9wq5dvGZvckSr3fxl50LBLV981Q4S4gVyQ67WGwFpFRq+PLSbk3GrgpCt5OKn+aPGmYo16KNRAdB04t89/1O/w1cDnyilFU=',
-//   // channelSecret: '1614fdc372087e9c6a6fc36829fad2f2'
-// }
-
-const app = express()
+const config = {
+  channelAccessToken: 'hQWQDyh2yengS8JSCPVb5JZZj+KhRuVHvRCL+W9XVNJnuvbwphLneuu3bcOz4UOOzSc1RSd8ZCqh9wq5dvGZvckSr3fxl50LBLV981Q4S4gVyQ67WGwFpFRq+PLSbk3GrgpCt5OKn+aPGmYo16KNRAdB04t89/1O/w1cDnyilFU=',
+  channelSecret: '1614fdc372087e9c6a6fc36829fad2f2'
+}
 
 app.get('/', function (req, res) {
     res.send('Hello World!!')
 })
-
-// app.post('/webhook', middleware(config), (req, res) => {
-//   console.log('webhook success')
-// })
+app.post('/webhook', middleware(config), (req, res) => {
+ console.log('webhook success')
+})
 
 app.set('port', (process.env.PORT || 4000))
 
